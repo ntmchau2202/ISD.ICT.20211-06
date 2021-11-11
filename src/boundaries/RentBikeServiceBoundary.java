035ee6c914d4dfa34d1b3b95e27a009e0cfc2012
Ngocngok <long.nt184287@gmail.com>
2021-11-11 22:33:58 +0700

    add docs for rent bike subsystem

--- src/boundaries/RentBikeServiceBoundary.java
+++ src/boundaries/RentBikeServiceBoundary.java
@@ -1,5 +1,19 @@
 package boundaries;
 
-public class RentBikeServiceBoundary {
+import exceptions.ecobike.EcoBikeUndefinedException;
+import exceptions.ecobike.RentBikeException;
+import interfaces.RentBikeServiceInterface;
 
+public class RentBikeServiceBoundary implements RentBikeServiceInterface {
+	public void rentBike(String bikeBarcode) throws RentBikeException, EcoBikeUndefinedException {
+	
+	}
+	
+	public void returnBike(String bikeBarcode) throws RentBikeException, EcoBikeUndefinedException {
+		
+	}
+	
+	public void pauseBikeRental(String bikeBarcode) throws RentBikeException, EcoBikeUndefinedException {
+		
+	}
 }
