77bc782fedc799f9283ddfb061e8cce42f47ede1
Minh Chau <chau.ntm184238@sis.hust.edu.vn>
2021-11-12 20:59:45 +0700

    move some functions from ReturnBikeController to RentBikeServiceController

--- src/controllers/RentBikeServiceController.java
+++ src/controllers/RentBikeServiceController.java
@@ -1,5 +1,25 @@
 package controllers;
 
 public class RentBikeServiceController {
-
+	/**
+	 * Steps to rent a bike
+	 * @param bikeBarcode the bar code of the bike to rent
+	 */
+	public void rentBike(String bikeBarcode) {
+		
+	}
+	
+	/**
+	 * Request to deposit
+	 */
+	public void requestToDeposit() {
+		
+	}
+	
+	/**
+	 * Start counting rental time when renting process finish
+	 */
+	public void startCountingRentalTime() {
+		
+	}
 }
