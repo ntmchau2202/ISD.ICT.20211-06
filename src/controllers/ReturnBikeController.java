7849c5c7e00df6d6ec475e7a72bace4266364615
Tran Le Hai Duong <duong.tlh.909@aptechlearning.edu.vn>
2021-11-13 14:16:21 +0700

    add:

---
+++ src/controllers/ReturnBikeController.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import exceptions.ecobike.EcoBikeUndefinedException;
+import exceptions.ecobike.RentBikeException;
+import interfaces.RentBikeInterface;
+
+public class ReturnBikeController implements RentBikeInterface {
+	public ReturnBikeController() {
+		
+	}
+
+	@Override
+	public void rentBike(String bikeBarcode) throws RentBikeException, EcoBikeUndefinedException {}
+	
+	@Override
+	public void pauseBikeRental(String bikeBarcode) throws RentBikeException, EcoBikeUndefinedException {}
+	
+	@Override
+	public void returnBike(String bikeBarcode) throws RentBikeException, EcoBikeUndefinedException {
+		// TODO Auto-generated method stub
+	}
+	
+	/**
+	 * Calculate the fee based on the time rent and deposit of the bike which got by searching barcode
+	 * @param  bikeBarcode Barcode of the bike to be rented
+	 * @throws RentBikeException If the bike is not currently available, the barcode is not valid
+	 * @throws EcoBikeUndefinedException If there is an unexpected error occurs during the renting process
+	 * @return a float value expresses the money have to pay in currency VND
+	 */
+	public double calculateFee(String bikeBarcode) throws RentBikeException, EcoBikeUndefinedException {
+		return 0;
+	}
+	
+	/**
+	 * Check the fee whether to pay or not
+	 * @param fee  The fee calculated before
+	 * @throws EcoBikeUndefinedException If there is an unexpected error occurs during the renting process
+	 * @return true/false
+	 */
+	public boolean checkHaveToPayOrNot(double fee) throws EcoBikeUndefinedException {
+		return true;
+	}
+	
+	/**
+	 * Request to pay for rental bike
+	 * @throws EcoBikeUndefinedException If there is an unexpected error occurs during the renting process
+	 */
+	public void requestToPay() throws EcoBikeUndefinedException {
+		
+	}
+}
--- src/views/PaymentMethodScreenHandler.java
+++ src/views/PaymentMethodScreenHandler.java
@@ -1,6 +1,9 @@
 package views;
 
 import controllers.EcoBikeBaseController;
+import entities.CreditCard;
+import exceptions.ecobike.EcoBikeUndefinedException;
+import exceptions.ecobike.RentBikeException;
 
 public class PaymentMethodScreenHandler extends EcoBikeBaseScreenHandler {
 
@@ -15,4 +18,38 @@ public class PaymentMethodScreenHandler extends EcoBikeBaseScreenHandler {
 		
 	}
 
+	/**
+	 * Confirm the payment method
+	 * @throws EcoBikeUndefinedException If there is an unexpected error occurs during the renting process
+	 */
+	public void confirmPaymentMethod() throws EcoBikeUndefinedException {
+		
+	}
+	
+	/**
+	 * Confirm the transaction
+	 * @throws RentBikeException If the card info is invalid
+	 * @throws EcoBikeUndefinedException If there is an unexpected error occurs during the renting process
+	 */
+	public void validateForm() throws RentBikeException, EcoBikeUndefinedException {
+		
+	}
+	
+	/**
+	 * Update the card info
+	 * @param card The credit card used to update
+	 * @throws RentBikeException If the card is invalid
+	 * @throws EcoBikeUndefinedException If there is an unexpected error occurs during the renting process
+	 */
+	public void updateCard(CreditCard card) throws RentBikeException, EcoBikeUndefinedException {
+		
+	}
+	
+	/**
+	 * display the error
+	 * @throws EcoBikeUndefinedException If there is an unexpected error occurs during the renting process
+	 */
+	public void notifyError() throws EcoBikeUndefinedException {
+		
+	}
 }
