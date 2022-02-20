9ff46f1b6c1666b701ffefe053596c7899138043
Minh Chau <chau.ntm184238@sis.hust.edu.vn>
2022-02-20 20:26:39 +0700

    Addjust diagrams, add installation guide

---
+++ InstallationGuide.md
@@ -0,0 +1,12 @@
+In order to run this application, you need to have
+
+- Java 11 
+
+- JavaFX 11 or above (We had provided JavaFX 15 in `/src/lib`)
+  
+- OpenJDK (for running javafx)
+
+- JSON library (with .jar file put in `/src/lib`); you'll need to import this in the module of the project before compiling
+
+- SQLite library (with .jar file put n `/src/lib`); you'll need to import this in the module of the project before compiling
+
--- reports/microsoftecobikesql.sql
+++ reports/microsoftecobikesql.sql
@@ -1,13 +1,13 @@
 --
--- File generated with SQLiteStudio v3.2.1 on T2 Thg1 17 10:01:12 2022
+-- File generated with SQLiteStudio v3.2.1 on CN Thg2 20 19:00:18 2022
 --
 -- Text encoding used: UTF-8
 --
-PRAGMA foreign_keys = off;
+-- PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
 -- Table: Administrator
-DROP TABLE IF EXISTS Administrator;
+-- DROP TABLE IF EXISTS Administrator;
 
 CREATE TABLE Administrator (
     username VARCHAR (256) NOT NULL
@@ -17,7 +17,7 @@ CREATE TABLE Administrator (
 
 
 -- Table: Bike
-DROP TABLE IF EXISTS Bike;
+-- DROP TABLE IF EXISTS Bike;
 
 CREATE TABLE Bike (
     name               VARCHAR (256) NOT NULL,
@@ -25,7 +25,6 @@ CREATE TABLE Bike (
     license_plate_code VARCHAR (32)  NOT NULL,
     bike_image         VARCHAR (256),
     bike_barcode       VARCHAR (10)  PRIMARY KEY,
-    bike_rental_price  FLOAT         NOT NULL,
     currency_unit      VARCHAR (3)   NOT NULL,
     create_date        DATE,
     creator            VARCHAR (256),
@@ -35,9 +34,20 @@ CREATE TABLE Bike (
     REFERENCES Administrator (username) 
 );
 
+-- Table: Dock
+-- DROP TABLE IF EXISTS Dock;
+
+CREATE TABLE Dock (
+    name         VARCHAR (256),
+    dock_id      INTEGER       PRIMARY KEY IDENTITY,
+    dock_address VARCHAR (256),
+    dock_area    FLOAT,
+    num_dock     INTEGER,
+    dock_image   VARCHAR (256) 
+);
 
 -- Table: BikeInDock
-DROP TABLE IF EXISTS BikeInDock;
+-- DROP TABLE IF EXISTS BikeInDock;
 
 CREATE TABLE BikeInDock (
     dock_id      INTEGER,
@@ -58,7 +68,7 @@ CREATE TABLE BikeInDock (
 
 
 -- Table: BikeStatus
-DROP TABLE IF EXISTS BikeStatus;
+-- DROP TABLE IF EXISTS BikeStatus;
 
 CREATE TABLE BikeStatus (
     bike_barcode    VARCHAR (10) NOT NULL
@@ -75,25 +85,29 @@ CREATE TABLE BikeStatus (
 );
 
 
--- Table: Dock
-DROP TABLE IF EXISTS Dock;
+-- Table: Configs
+-- DROP TABLE IF EXISTS Configs;
 
-CREATE TABLE Dock (
-    name         VARCHAR (256),
-    dock_id      INTEGER       PRIMARY KEY AUTOINCREMENT,
-    dock_address VARCHAR (256),
-    dock_area    FLOAT,
-    num_dock     INTEGER,
-    dock_image   VARCHAR (256) 
+CREATE TABLE Configs (
+    bike_type   VARCHAR (256) PRIMARY KEY,
+    bike_price  FLOAT,
+    saddles     INTEGER,
+    pedals      INTEGER,
+    motors      INTEGER,
+    rear_seats  INTEGER,
+    rent_factor FLOAT
 );
 
 
+
+
+
 -- Table: EcoBikeTransaction
-DROP TABLE IF EXISTS EcoBikeTransaction;
+-- DROP TABLE IF EXISTS EcoBikeTransaction;
 
 CREATE TABLE EcoBikeTransaction (
     transaction_id     INTEGER       NOT NULL
-                                     PRIMARY KEY AUTOINCREMENT,
+                                     PRIMARY KEY IDENTITY,
     transaction_amount FLOAT         NOT NULL,
     transaction_time   VARCHAR (256) NOT NULL,
     transaction_detail VARCHAR (256),
@@ -101,35 +115,12 @@ CREATE TABLE EcoBikeTransaction (
     rent_id            INTEGER
 );
 
-
--- Table: Invoice
-DROP TABLE IF EXISTS Invoice;
-
-CREATE TABLE Invoice (
-    invoice_id     INTEGER       NOT NULL,
-    transaction_id INTEGER       NOT NULL
-                                 PRIMARY KEY,
-    rent_id        INTEGER       NOT NULL
-                                 REFERENCES CustomerRent (rent_id),
-    date_issued    VARCHAR (256),
-    CONSTRAINT FK_Invoice_Transaction FOREIGN KEY (
-        transaction_id
-    )
-    REFERENCES EcoBikeTransaction (transaction_id),
-    CONSTRAINT FK_Invoice_Customer FOREIGN KEY (
-        rent_id
-    )
-    REFERENCES Customer (customer_id) 
-);
-
-
 -- Table: RentBike
-DROP TABLE IF EXISTS RentBike;
+-- DROP TABLE IF EXISTS RentBike;
 
 CREATE TABLE RentBike (
     rent_id      INTEGER       NOT NULL
-                               REFERENCES CustomerRent (rent_id) 
-                               PRIMARY KEY AUTOINCREMENT,
+                               PRIMARY KEY IDENTITY,
     bike_barcode VARCHAR (10)  NOT NULL,
     start_time   VARCHAR (256) NOT NULL,
     end_time     VARCHAR (256),
@@ -138,12 +129,25 @@ CREATE TABLE RentBike (
         bike_barcode
     )
     REFERENCES Bike (bike_barcode),
-    CONSTRAINT FK_RenBike_Customer FOREIGN KEY (
-        rent_id
-    )
-    REFERENCES Customer (customer_id) 
 );
 
 
+-- Table: Invoice
+-- DROP TABLE IF EXISTS Invoice;
+
+CREATE TABLE Invoice (
+    invoice_id     INTEGER       NOT NULL,
+    transaction_id INTEGER       NOT NULL,
+    rent_id        INTEGER       NOT NULL,
+    date_issued    VARCHAR (256),
+	CONSTRAINT PK_Rent_Trans_ID PRIMARY KEY(transaction_id, rent_id),
+    CONSTRAINT FK_Invoice_Transaction FOREIGN KEY (transaction_id) REFERENCES EcoBikeTransaction(transaction_id),
+    CONSTRAINT FK_Invoice_Customer FOREIGN KEY (rent_id) REFERENCES RentBike(rent_id),
+);
+
+
+
+
+
 COMMIT TRANSACTION;
-PRAGMA foreign_keys = on;
+-- PRAGMA foreign_keys = on;
